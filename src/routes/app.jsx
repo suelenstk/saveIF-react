ef8356cc91c4df552f655d84fa55d5db451e58cc
eduarda <eduarda@eduarda-Aspire-5733>
2018-04-17 21:12:53 -0300

    Tela inscrever-se em grupos e tela visualizar posts do grupo.

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -1,14 +1,19 @@
 import UserProfile from '../views/UserProfile/UserProfile';
 import Groups from '../views/Groups/Groups';
-import Home from '../views/Home/Home'
+import Home from '../views/Home/Home';
 import UserRegistration from "../views/UserRegistration/UserRegistration";
+import PostsView from '../views/Groups/PostsView';
+import GroupEnter from '../views/Groups/GroupEnter';
 
 
+ 
 const appRoutes = [
     { path: "/home", name: "Home", icon: "pe-7s-home", component: Home},
     { path: "/user", name: "Meu perfil", icon: "pe-7s-user", component: UserProfile },
     { path: "/notifications", name: "Notificações", icon: "pe-7s-bell" },
     { path: "/groups", name: "Grupos", icon: "pe-7s-share", component: Groups },
+    { path: "/GroupEnter", name: "GroupEnter", icon: "pe-7s-share", component: GroupEnter },
+    { path: "/PostsView", name: "PostsView", icon: "pe-7s-share", component: PostsView },
     { redirect: true, path:"/", to:"/home", name: "Home" }
 ];
 
---
+++ src/views/Groups/GroupEnter.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+import Card from '../../components/Card/Card';
+import Button from '../../elements/CustomButton/CustomButton.jsx';
+
+class GroupEnter extends Component {
+    
+    render() {
+        return (
+            <div className="content">
+                <Grid fluid>
+                    <Row>
+                        <Col md={12}>
+                            <Card
+                                ctAllGroups
+                            
+                                content={
+                                    <Row>
+
+                                        <Col lg={12} md={12} sm={12} xs={12} >
+
+                                            <h5>Nome do Grupo</h5>
+
+                                            <h3>Algoritmos</h3>
+                                            <br/>
+                                            <h5>Descrição</h5>
+
+                                            <p>Algoritmo ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                            <br/>
+                                            <h5>Categorias</h5>
+                                            <p>Programação JAVA</p>
+                                            <br/>
+                                            <h5>Participantes</h5>
+                                                       
+                                        
+
+                                             <Button
+                                                bsStyle="danger"
+                                                pullRight
+                                                fill
+                                                type="submit"                                           
+                                            >   
+                                                Solicitar Inscrição
+                                            </Button>                                                        
+                                            <br/><br/>
+                                            <hr/>
+                                        </Col>
+                                        
+                                    </Row>
+                                }
+                            />
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default GroupEnter;
