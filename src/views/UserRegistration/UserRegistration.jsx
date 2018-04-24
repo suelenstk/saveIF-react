59cd06d2d1eaa3ab7aee1c3cd45f51ca46a3f9c1
giordano <buffonggb@gmail.com>
2018-04-24 20:13:11 -0300

    Validações no cadastro de usuários

--- src/views/UserRegistration/UserRegistration.jsx
+++ src/views/UserRegistration/UserRegistration.jsx
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Col, ControlLabel, FormControl, FormGroup, Grid, Row} from 'react-bootstrap';
 
 import {Card} from '../../components/Card/Card.jsx';
 import Button from '../../elements/CustomButton/CustomButton.jsx';
+import UserService from '../../services/UserService';
 import Navbar from "react-bootstrap/es/Navbar";
 import logo from '../../assets/img/reactlogo.png';
 
@@ -31,6 +32,30 @@ class UserRegistration extends Component {
     }
 
     render() {
+        let curso = null;
+
+        if (this.state.usuario.tipoVinculo === "aluno") {
+            curso =
+                <Row>
+                    <FormGroup controlId="formControlSelectCurso" className="col-md-12">
+                        <ControlLabel>Curso</ControlLabel>
+                        <FormControl
+                            componentClass="select"
+                            placeholder="curso"
+                            value=""
+                            onChange=""
+                        >
+                            <option value="">-- Selecione --</option>
+                            {/*{this.props.listaDeCursos.content.map((curso) => {*/}
+                            {/*return <option value={curso.id}>{curso.nome}</option>*/}
+                            {/*})}*/}
+                        </FormControl>
+                    </FormGroup>
+                </Row>
+        } else {
+            curso = "";
+        }
+
         return (
             <div className="wrapper">
                 <Navbar className="navbarLogin">
@@ -45,15 +70,18 @@ class UserRegistration extends Component {
                                 <Card
                                     title="Cadastro de usuário"
                                     content={
-                                        <form>
+                                        <form onSubmit={(event) => {
+                                            event.preventDefault();
+                                            this.confirmar()
+                                        }}>
                                             <Row>
                                                 <FormGroup controlId="formHorizontalPrefixoEmail" className="col-md-6">
                                                     <ControlLabel>Prefixo do e-mail</ControlLabel>
                                                     <FormControl
                                                         type="text"
-                                                        value={this.state.usuario.prefixoEmail}
+                                                        value={this.state.usuario.email}
                                                         placeholder="Ex: pmachado"
-                                                        onChange={(e) => this.setValor("prefixoEmail", e.target.value)}
+                                                        onChange={(e) => this.setValor("email", e.target.value)}
                                                         required
                                                     />
                                                 </FormGroup>
@@ -79,9 +107,9 @@ class UserRegistration extends Component {
                                                     <ControlLabel>Senha</ControlLabel>
                                                     <FormControl
                                                         type="password"
-                                                        value={this.state.usuario.senha}
+                                                        value={this.state.usuario.novaSenha}
                                                         placeholder="Senha"
-                                                        onChange={(e) => this.setValor("senha", e.target.value)}
+                                                        onChange={(e) => this.setValor("novaSenha", e.target.value)}
                                                         required
                                                     />
                                                 </FormGroup>
@@ -90,21 +118,21 @@ class UserRegistration extends Component {
                                                     <ControlLabel> Confirmação de senha</ControlLabel>
                                                     <FormControl
                                                         type="password"
-                                                        value={this.state.usuario.confSenha}
+                                                        value={this.state.confirmaSenha}
                                                         placeholder="Confirmação de senha"
-                                                        onChange={(e) => this.setValor("confSenha", e.target.value)}
+                                                        onChange={(e) => this.setState({confirmaSenha: e.target.value})}
                                                         required
                                                     />
                                                 </FormGroup>
                                             </Row>
                                             <Row>
-                                                <FormGroup controlId="formControlSelectVinculo" className="col-md-6">
+                                                <FormGroup controlId="formControlSelectVinculo" className="col-md-12">
                                                     <ControlLabel>Vínculo</ControlLabel>
                                                     <FormControl
                                                         componentClass="select"
                                                         placeholder="vinculo"
-                                                        value={this.state.usuario.vinculo}
-                                                        onChange={(evento) => this.setValor("vinculo", evento.target.value)}
+                                                        value={this.state.usuario.tipoVinculo}
+                                                        onChange={(evento) => this.setValor("tipoVinculo", evento.target.value)}
                                                         required>
                                                         <option value="">-- Selecione --</option>
                                                         <option value="aluno">Aluno</option>
@@ -113,31 +141,17 @@ class UserRegistration extends Component {
                                                     </FormControl>
                                                 </FormGroup>
                                             </Row>
-                                            <Row>
-                                                <FormGroup controlId="formControlSelectCurso" className="col-md-12">
-                                                    <ControlLabel>Curso</ControlLabel>
-                                                    <FormControl
-                                                        componentClass="select"
-                                                        placeholder="curso"
-                                                        value={this.state.usuario.curso}
-                                                        onChange={(evento) => this.setValor("curso", evento.target.value)}
-                                                        required>
-                                                        <option value="">-- Selecione --</option>
-                                                        {/*{this.props.listaDeCursos.content.map((curso) => {*/}
-                                                        {/*return <option value={curso.id}>{curso.nome}</option>*/}
-                                                        {/*})}*/}
-
-                                                    </FormControl>
-                                                </FormGroup>
-                                            </Row>
+                                            {curso}
                                             <Row>
                                                 <Col md={12}>
                                                     <FormGroup controlId="formControlsTextarea">
-                                                        <ControlLabel>Sobre mim</ControlLabel>
+                                                        <ControlLabel>Sobre mim (opcional)</ControlLabel>
                                                         <FormControl
                                                             rows="5" componentClass="textarea"
                                                             bsClass="form-control"
                                                             placeholder="Fale um pouco sobre você..."
+                                                            value={this.state.usuario.sobreUsuario}
+                                                            onChange={(e) => this.setValor("sobreUsuario", e.target.value)}
                                                         />
                                                     </FormGroup>
                                                 </Col>
