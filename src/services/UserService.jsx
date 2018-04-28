137c5f7651d56ebf508b70bdcf67a21d83903fa1
suelenstk <sstrack@restiga.ifrs.edu.br>
2018-04-28 08:16:26 -0300

    1. Cadastrar Usuários - Serviço React (Correção em serviço)

--- src/services/UserService.jsx
+++ src/services/UserService.jsx
@@ -1,33 +1,8 @@
 import ServicoRest from "../ServicoRest";
-import servicoLogin from "../login/ServicoLogin";
 
-class UserService  {
+export default class UserService extends  ServicoRest {
         constructor(){
-           
+            super("api/usuarios/");
         }
-
-
-        listarNaoPaginado(sucesso, erro) {
-
-            let trataFetch = (resultado) => {
-                if (resultado.ok) {
-                    resultado.json().then(sucesso)
-                } else {
-                    resultado.json().then(
-                        (resultadoErro) => erro(resultadoErro)
-                    )
-                }
-            };
-    
-            fetch("/api/usuarios/listar", {
-                headers: new Headers({
-                    'Authorization': servicoLogin.getAuthorization(),
-                }),
-                method: "GET"
-            }).then(trataFetch);
-        }
-    
-    }
-    
-let userService = new UserService();        
-export default UserService;
+        
+}
