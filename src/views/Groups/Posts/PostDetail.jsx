bf173b81fdbbb3323bc0c61254ebed879a04163b
eduarda <eduarda@eduarda-Aspire-5733>
2018-04-29 18:49:32 -0300

    Tela base anexo.

--- src/views/Groups/Posts/PostDetail.jsx
+++ src/views/Groups/Posts/PostDetail.jsx
@@ -1,52 +1,56 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Image } from 'react-bootstrap';
 import Card from '../../../components/Card/Card';
+import anexoTeste from '../../../img/anexoTeste.JPG';
 
+    const imgStyle = {
+    float: 'right',
+    marginRight: '55px',
+  };
 
 export default class PostList extends React.Component{
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            post: this.props.post
-        }       
+   constructor(props) {    
+     super(props);
+     this.state = {
+     post: this.props.post
+        } 
     }
 
     render(){
 
         return(
 
-            <div className="content">
+          <div className="content">
+               <Card
+            title={this.state.post.titulo}
+            ctAllGroups
+            content={
             
-                <Card
-                    title={this.state.post.titulo}
-                    ctAllGroups
-                    content={
-                        <Row>
-                        
-                            <Col lg={12} md={12} sm={12} xs={12}>
-
-                                <h5><span className="glyphicon glyphicon-time"></span> Post by {this.state.post.autorPost.nome}, {this.state.post.dataPostagem}.</h5>
-                                <p>{this.state.post.texto}</p>
-
-
-                            </Col>
-                        
-                        </Row>
-
-                    }
-                />
-            
-            
-            
+            <Row>
+    
+            <Col lg={12} md={12} sm={12} xs={12}>
+            <div style={imgStyle}>
+                <Image src={anexoTeste} responsive/>
+                <p style={{textAling:'center'}}>{this.state.post.anexoPost.nomeAnexo}</p>
             </div>
+  
+                <h5><span className="glyphicon glyphicon-time"></span> Post by {this.state.post.autorPost.nome}, {this.state.post.dataPostagem}.</h5>
+                <p>{this.state.post.texto}</p> 
+            </Col>
+            
+                </Row>
 
-        );
+                }
+          />
+    </div>
+
+);
 
 
 
 
-    }
+}
 
 
 
