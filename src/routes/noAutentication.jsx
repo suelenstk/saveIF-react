70db8e628af70bc9cfd1d8785abb5161839c4c89
suelenstk <sstrack@restiga.ifrs.edu.br>
2018-04-29 00:00:32 -0300

    1. Cadastrar Usuários - Serviço React & 0. Tela inicial do sistema (Rotas detalhe login)

--- src/routes/noAutentication.jsx
+++ src/routes/noAutentication.jsx
@@ -2,11 +2,7 @@ import UserRegistration from "../views/UserRegistration/UserRegistration";
 import Login from '../views/Login/Login';
 
 const noAutenticationRoutes = [
-    { path: "/login", name:"Login", component: Login },
-    
     { path: "/cadastro", name:"Cadastro", component: UserRegistration },
-
-   
     { redirect: true, path:"/", to:"/login", name: "Login" }
 ];
 
