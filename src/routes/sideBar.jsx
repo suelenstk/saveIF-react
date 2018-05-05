588b7f2e68dfb860c653b66eda631f852dc4895c
suelenstk <sstrack@restiga.ifrs.edu.br>
2018-05-05 09:15:48 -0300

    Ícones da barra lateral

--- src/routes/sideBar.jsx
+++ src/routes/sideBar.jsx
@@ -11,10 +11,9 @@ const appRoutes = [
    { path: "/home", name: "Home", icon: "pe-7s-home", component: Home},
    { path: "/user", name: "Meu perfil", icon: "pe-7s-user", component: UserProfile },
    { path: "/notifications", name: "Notificações", icon: "pe-7s-bell" },
-   { path: "/groups", name: "Grupos", icon: "pe-7s-share", component: Groups },
-   { path: "/MyGroups", name: "Meus grupos", icon: "pe-7s-share", component: MyGroups },
-   { path: "/NewPost", name: "Novo Post", icon: "pe-7s-file", component: NewPost },
-   { path: "/GroupPage", name: "GroupPage", icon: "pe-7s-file", component: GroupPage }
+   { path: "/groups", name: "Grupos", icon: "pe-7s-help2", component: Groups },
+   { path: "/MyGroups", name: "Meus grupos", icon: "pe-7s-star", component: MyGroups },
+   { path: "/GroupPage", name: "Novo Grupo", icon: "pe-7s-plus", component: GroupPage }
 ];
 
 
