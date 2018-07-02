fe4258a1a9af116208a950b44951504aaa7480c9
suelenstk <sstrack@restiga.ifrs.edu.br>
2018-07-02 03:12:53 -0300

    2.7 Tornar participante um Coordenador - componente de tela + serviço React

--- src/views/Groups/ListParticipants.jsx
+++ src/views/Groups/ListParticipants.jsx
@@ -35,29 +35,65 @@ class ListParticipants extends Component {
             }
         );
     }
+    verificaCoordenador(usuarioLista) {  
+        let coordena = true;
+        this.props.coordenadoresGrupo.map((usuario) => {            
+            if (usuario.id === usuarioLista.id) {
+                coordena = false;
+            }
+            
+        });
+        return coordena;
+    }
+    coordenaGrupo(usuario) {
+        this.groupService.tornarCoordenador(this.state.idGrupo, usuario.id,
+            (sucesso) => {
+                alert("Usuário (a) " + usuario.nome + " agora é um(a) coordenador(a)!");
+                window.location.reload ();
+            },
+            (erro) => {
+                console.log("Erro!");
+                console.log(erro);
+                alert (erro.message);
+            }
+        );
+    }
 
     render() {
         if (this.props.pagina.content) {
             console.log(this.props.pagina);
-            return <div className="content">
-                {this.props.pagina.content.map((usuario) => {
-                    return <UserChip
-                        value={usuario.id}
-                        key={usuario.id}
-                        nome={usuario.nome}
-                        avatar={avatar}
-                        alt={usuario.nome}
-                        class="addUserbtn"
-                        largura="30%"
-                        icone={this.props.icone}
-                        colorIcon={this.state.colorIcon}
-                        evento={() => {
-                            this.removeGrupo(usuario)
-                        }}
-                    />
-                })}
-            </div>
 
+            if (this.props.coordenadoresGrupo) {
+                return <div className="content">
+                    {this.props.pagina.content.map((usuario) => {
+                        let coordena = this.verificaCoordenador(usuario);
+                        
+                        return <UserChip
+                            value={usuario.id}
+                            key={usuario.id}
+                            nome={usuario.nome}
+                            avatar={avatar}
+                            alt={usuario.nome}
+                            class="addUserbtn"
+                            largura="30%"
+                            colorIcon={this.state.colorIcon}
+                            
+                            icone1={this.props.icone}                        
+                            evento1={() => {
+                                this.removeGrupo(usuario)
+                            }}
+                            nomeEvento1={"Remover Participante"}
+                            
+                            icone2={"pe-7s-star"}
+                            evento2={() => {
+                                this.coordenaGrupo(usuario)
+                            }}
+                            nomeEvento2={"Tornar Coordenador"}
+                            eventoDisplay2={coordena}
+                        />
+                    })}
+                </div>
+            }
 
         } else {
             return <div></div>
